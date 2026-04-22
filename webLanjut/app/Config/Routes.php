d028a33ed005f5451086f5ee9dff962fe3299f72
wilma <mornlitoralis@gmail.com>
2026-04-22 08:04:37 +0700

    finishing filter

--- webLanjut/app/Config/Routes.php
+++ webLanjut/app/Config/Routes.php
@@ -5,6 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/produk', 'ProduksiController::index');
-$routes->get('/keranjang', 'TransaksiController::index');
+$routes->get('/', 'Home::index'); //mengakses rute ini harus login dulu
+
+$routes->get('login', '\App\Controllers\AuthController::login');
+$routes->post('login', '\App\Controllers\AuthController::login');
+$routes->get('logout', '\App\Controllers\AuthController::logout');
+
+$routes->get('/produk', 'ProduksiController::index', ['filter' => 'auth']); //mengakses rute ini harus login dulu
+$routes->get('/keranjang', 'TransaksiController::index', ['filter'=>'auth']); //mengakses rute ini harus login dulu
