72871b1633743303ba94410cb12c1fe26d008d71
wilma <mornlitoralis@gmail.com>
2026-05-20 10:22:27 +0700

    Configure the home controller using model product to display product data on the Home

--- webLanjut/app/Controllers/Home.php
+++ webLanjut/app/Controllers/Home.php
@@ -2,11 +2,20 @@
 
 namespace App\Controllers;
 
+use App\Models\ProductModel; //Import productModel
 class Home extends BaseController
 {
+    protected $productModel;
+
+    function __construct(){
+        $this->productModel = new ProductModel();
+    }
+
     public function index(): string
     {
-        return view('v_home');
+        $products = $this->productModel->findAll(); //ambil semua data dari product menggunakan fungsi findAll()
+        $data['products'] = $products; //view menerima data dalam bentuk array 
+        return view('v_home', $data);
     }
     public function contact(): string{
         return view('v_contact');
