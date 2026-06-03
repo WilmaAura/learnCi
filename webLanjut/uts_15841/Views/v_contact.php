4635dcdb7146df67ee262164cb122f623c4176ee
wilma <mornlitoralis@gmail.com>
2026-06-03 08:10:09 +0700

    add external library

--- webLanjut/uts_15841/Views/v_contact.php
+++
@@ -1,4 +0,0 @@
-<?= $this->extend('layout') ?>
-<?= $this->section('content') ?>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur nam ad, tenetur dolore mollitia pariatur consectetur dolorum hic dolores quidem. Officia repudiandae asperiores aperiam, tempora similique voluptate perspiciatis quos laboriosam?
-<?= $this->endSection() ?>
