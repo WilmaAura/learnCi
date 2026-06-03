d4c25e28a98449be818112f5c56f65e5baa6e5c2
wilma <mornlitoralis@gmail.com>
2026-06-03 11:53:08 +0700

    Some change on index and add construct, function cart_add from external library

--- webLanjut/app/Controllers/TransaksiController.php
+++ webLanjut/app/Controllers/TransaksiController.php
@@ -7,8 +7,32 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class TransaksiController extends BaseController
 {
+    protected $cart;
+    public function __construct(){
+        helper(['number', 'form']);
+        $this->cart= service('cart');
+    }
     public function index()
     {
-        return view('v_keranjang');
+        $data = [
+            'items' => $this->cart->contents()
+        ];
+        return view('v_keranjang', $data);
+    }
+
+    public function cart_add(){
+       $this->cart->insert([
+        'id'      => $this->request->getPost('id'),
+        'qty'     => 1,
+        'price'   => $this->request->getPost('harga'),
+        'name'    => $this->request->getPost('nama'),
+        'options' => [
+            'foto' => $this->request->getPost('foto')
+          ] 
+        ]); 
+        session()->setFlashdata(
+            'success',
+            
+        );
     }
 }
