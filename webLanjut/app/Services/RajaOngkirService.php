58f6b69d6f81b3873c7ab1737659c15bc089fec9
wilma <mornlitoralis@gmail.com>
2026-06-23 19:15:32 +0700

    Use webservice from rajaongkir

---
+++ webLanjut/app/Services/RajaOngkirService.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Services;
+use Config\Services;
+
+class RajaOngkirService
+{
+    protected $client;
+    protected $apiKey;
+    protected $baseurl;
+
+    public function __construct(){
+        $this->client = Services::curlrequest([
+            'timeout' => 10,
+            'http_errors' => false,
+        ]);
+        $this->apiKey = env('RAJAONGKIR_API_KEY');
+        $this->baseUrl = env('RAJAONGKIR_BASE_URL');
+    }
+    //Untuk mendapatkan data daerah tujuan 
+    public function getDestination(string $keyword): array{
+        $response = $this->client->get(
+            $this->baseUrl.'destination/domestic-destination',[
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'key' => $this->apiKey,
+                ],
+                'query' => [
+                    'search' => $keyword,
+                    'limit' => 50,
+                ]
+            ]
+        );
+        return json_decode(
+            $response->getBody(),
+            true
+        );
+    }
+    //Mendapatkan data layanan serta ongkos kirimnya 
+    public function getCost(string $origin, string $destination, int $weight, string $courier): array{
+        $response = $this->client->post(
+            $this->baseUrl . 'calculate/domestic-cost',
+            [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'key'    => $this->apiKey,
+                ],
+                'form_params' => [
+                    'origin'      => $origin,
+                    'destination' => $destination,
+                    'weight'      => $weight,
+                    'courier'     => $courier,
+                ]
+            ]
+        );
+
+        return json_decode(
+            $response->getBody(),
+            true
+        );;
+    }
+}
+?>
