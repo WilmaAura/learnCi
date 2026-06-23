383d796b0db09f7268775c5fab7859b525143f5e
wilma <mornlitoralis@gmail.com>
2026-06-23 16:15:13 +0700

    Changes on checkout

--- webLanjut/app/Views/v_checkout.php
+++ webLanjut/app/Views/v_checkout.php
@@ -1,9 +1,93 @@
 <?= $this->extend('layout') ?>
 <?= $this->section('content')?>
 
-<?php
-d($items);
-d($total);
-?>
+<div class="row">
+    <div class="col-lg-6">
+        <?= form_open('buy', 'class="row g-3"') ?>
+
+<?= form_hidden('username', session()->get('username')) ?>
+<?= form_hidden('total_harga', '') ?>
+
+<div class="col-12">
+    <?= form_label('Nama', 'nama', ['class' => 'form-label']) ?>
+    <?= form_input([
+        'name'     => 'nama',
+        'id'       => 'nama',
+        'class'    => 'form-control',
+        'value'    => session()->get('username'),
+        'readonly' => true]) ?>
+</div>
+<div class="col-12">
+    <?= form_label('Alamat', 'alamat', ['class' => 'form-label']) ?>
+    <?= form_input([
+        'name'  => 'alamat',
+        'id'    => 'alamat',
+        'class' => 'form-control']) ?>
+</div> 
+<div class="col-12"> 
+    <?= form_label('Kelurahan', 'kelurahan', ['class' => 'form-label']) ?>
+    <strong>select kelurahan</strong>
+</div>
+<div class="col-12"> 
+    <?= form_label('Layanan', 'layanan', ['class' => 'form-label']) ?> 
+    <strong>select layanan</strong>
+</div>
+<div class="col-12">
+    <?= form_label('Ongkir', 'ongkir', ['class' => 'form-label']) ?>
+    <?= form_input([
+        'name'     => 'ongkir',
+        'id'       => 'ongkir',
+        'class'    => 'form-control',
+        'readonly' => true]) ?>
+</div>
+<div class="col-12">
+    <?= form_submit(
+        'submit',
+        'Buat Pesanan',
+        ['class' => 'btn btn-primary']) ?>
+</div>
+
+<?= form_close() ?> 
+    </div>
+    <div class="col-lg-6">
+        <table class="table">
+  <thead>
+      <tr>
+          <th scope="col">Nama</th>
+          <th scope="col">Harga</th>
+          <th scope="col">Jumlah</th>
+          <th scope="col">Sub Total</th>
+      </tr>
+  </thead>
+  <tbody>
+      <?php 
+      if (!empty($items)) :
+          foreach ($items as $index => $item) :
+      ?>
+              <tr>
+                  <td><?= $item['name'] ?></td>
+                  <td><?= number_to_currency($item['price'], 'IDR') ?></td>
+                  <td><?= $item['qty'] ?></td>
+                  <td><?= number_to_currency($item['price'] * $item['qty'], 'IDR') ?></td>
+              </tr>
+      <?php
+          endforeach;
+      endif;
+      ?>
+      <tr>
+          <td colspan="2"></td>
+          <td>Subtotal</td>
+          <td><?= number_to_currency($total, 'IDR') ?></td>
+      </tr>
+      <tr>
+          <td colspan="2"></td>
+          <td>Total</td>
+          <td><span id="total"><?= number_to_currency($total, 'IDR') ?></span></td>
+      </tr>
+  </tbody>
+</table>
+    </div>
+</div>
+</div>
 
 <?=$this->endSection() ?>
