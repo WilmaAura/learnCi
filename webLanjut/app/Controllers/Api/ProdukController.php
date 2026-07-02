bb67033cf4508ac16c175b4afcdec9e010468274
wilma <mornlitoralis@gmail.com>
2026-07-02 12:25:58 +0700

    200~menambahkan api data produk dan transaksi

--- webLanjut/app/Controllers/Api/ProdukController.php
+++ webLanjut/app/Controllers/Api/ProdukController.php
@@ -4,19 +4,64 @@ namespace App\Controllers\Api;
 
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\RESTful\ResourceController;
+use App\Models\ProductModel;
 
 class ProdukController extends ResourceController
 {
+
+    protected $model;
+    private $token;
+
+    function __construct(){
+        $this->model = new ProductModel();
+        $this->token = env('MY_API_KEY');
+    }
+    //Functions untuk pengecekan API KEY yang tersimpan pada file .env
+   private function authenticate(){
+    $header = $this->request->getHeaderLine('Authorization');
+    if (empty($header)) {
+        return false;
+    }
+    if (!preg_match('/Bearer\s+(.*)$/i', $header, $matches)) {
+        return false;
+    }
+    return $matches[1] === $this->token;
+}
+    private function unauthorized(){
+        return $this->respond([
+            'status' => false,
+            'message' => 'Unauthorized'
+        ], 401);
+    }
+    // End of functions pengecekan
     /**
      * Return an array of resource objects, themselves in array format.
      *
      * @return ResponseInterface
      */
     public function index()
-    {
-        //
+{
+    if (!$this->authenticate()) {
+        return $this->unauthorized();
     }
 
+    $page = (int) ($this->request->getGet('page') ?? 1);
+    $perPage = (int) ($this->request->getGet('per_page') ?? 10);
+
+    $products = $this->model->paginate($perPage, 'default', $page);
+
+    return $this->respond([
+        'data' => $products,
+        'pagination' => [
+            'current_page' => $page,
+            'per_page'     => $perPage,
+            'last_page'    => $this->model->pager->getPageCount(),
+            'total_data'   => $this->model->pager->getTotal(),
+            'has_next'     => $page < $this->model->pager->getPageCount(),
+            'has_prev'     => $page > 1,
+        ]
+    ]);
+}
     /**
      * Return the properties of a resource object.
      *
@@ -26,7 +71,14 @@ class ProdukController extends ResourceController
      */
     public function show($id = null)
     {
-        //
+        if (!$this->authenticate()) {
+        return $this->unauthorized();
+    }
+        $product = $this->model->find($id);
+        if (!$product) {
+            return $this->failNotFound('Produk tidak ditemukan');
+        }
+        return $this->respond($product);
     }
 
     /**
@@ -34,8 +86,7 @@ class ProdukController extends ResourceController
      *
      * @return ResponseInterface
      */
-    public function new()
-    {
+    public function new(){
         //
     }
 
@@ -44,9 +95,15 @@ class ProdukController extends ResourceController
      *
      * @return ResponseInterface
      */
-    public function create()
-    {
-        //
+    public function create(){
+        if (!$this->authenticate()) {
+        return $this->unauthorized();
+    }
+        $data = $this->request->getJSON(true);
+        $this->model->insert($data);
+        return $this->respondCreated([
+            'message' => 'Produk berhasil ditambahkan'
+        ]);
     }
 
     /**
@@ -68,9 +125,18 @@ class ProdukController extends ResourceController
      *
      * @return ResponseInterface
      */
-    public function update($id = null)
-    {
-        //
+    public function update($id = null){
+        if (!$this->authenticate()) {
+        return $this->unauthorized();
+    }
+        if (!$this->model->find($id)) {
+            return $this->failNotFound('Produk tidak ditemukan');
+        }
+        $data = $this->request->getJSON(true);
+        $this->model->update($id, $data);
+        return $this->respond([
+            'message' => 'Produk berhasil diperbarui'
+        ]);
     }
 
     /**
@@ -80,8 +146,16 @@ class ProdukController extends ResourceController
      *
      * @return ResponseInterface
      */
-    public function delete($id = null)
-    {
-        //
+    public function delete($id = null){
+        if (!$this->authenticate()) {
+         return $this->unauthorized();
+        }
+        if (!$this->model->find($id)) {
+            return $this->failNotFound('Produk tidak ditemukan');
+        }
+        $this->model->delete($id);
+        return $this->respondDeleted([
+            'message' => 'Produk berhasil dihapus'
+        ]);
     }
 }
